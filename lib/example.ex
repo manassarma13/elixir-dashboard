1e32f7dae85a217942c47e6e461abbbf7d3c0407
Manas Sarma <manassarma@Manass-Laptop.local>
2024-09-05 23:07:22 +0530

    init commit

--- lib/example.ex
+++ lib/example.ex
@@ -1,11 +1,18 @@
-defmodule Example do
-  use Application
-  def start(_type, _args) do
-    IO.puts(Example.flying())
-    Supervisor.start_link([], strategy: :one_for_one)
+defmodule Example.Worker do
+  def start_link(_args) do
+    Task.start_link(fn -> perform_task() end)
   end
 
-  def flying do
-    :whale
+  def perform_task do
+    IO.puts("Performing background task...")
+    :timer.sleep(1000)
+    IO.puts("Task finished.")
   end
+end
+
+def start(_type, _args) do
+  children = [
+    {Example.Worker, []}
+  ]
+  Supervisor.start_link(children, strategy: :one_for_one)
 end
